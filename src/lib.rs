0f70ff9444c7a311dd5bb102d244e86432e2f502
Nathan Fritzler <nfblaster@live.com>
2021-09-21 12:37:40 -0600

    Move repl out of main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+//TODO split this into two files
+pub mod repl{
+    #[derive(Debug, Clone, Copy)]
+    pub enum ReplSource{
+        User,
+        File,
+    }
+    fn read(source:ReplSource,last_errors:ReplError)->Option<String>{
+        todo!("Get the code that needs to run! {:?} {:?}", source, last_errors)
+    }
+    fn eval(string:String)->ReplError{
+        todo!("Run the code! {}",string)
+    }
+    #[derive(Debug)]
+    pub enum ReplError{
+        ErrorCodes(Vec<i32>),
+        SyntaxError(String)
+    }
+    pub fn repl(source:ReplSource)->Result<(),ReplError>{
+        let mut repl_err=ReplError::ErrorCodes(Vec::new());
+        loop{
+            match read(source,repl_err){
+                Some(data)=>{
+                    repl_err=eval(data);
+                    match source{
+                        ReplSource::File=>{
+                            match &repl_err{
+                                ReplError::ErrorCodes(codes)=>if codes.is_empty(){
+                                    continue
+                                },
+                                ReplError::SyntaxError(..)=>{}
+                            }
+                            return Err(repl_err)
+                        }
+                        //The user will see the error on the next call to read, if there is one.
+                        ReplSource::User=>{}
+                    }
+                },
+                None=>return Ok(())
+            }
+        }
+    }
+}
 pub mod arguments{
     use std::env;
     pub struct Arguments{
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,8 @@
-use std::process;
+use std::process::exit;
 use lmsh::arguments::Arguments;
+use lmsh::repl::{repl, ReplSource};
 fn greet(){
     println!("Welcome to Lazerbeak12345's Micro Shell!");
-}
-#[derive(Debug, Clone, Copy)]
-enum ReplSource{
-    User,
-    File,
-}
-fn read(source:ReplSource,last_errors:ReplError)->Option<String>{
-    todo!("Get the code that needs to run! {:?} {:?}", source, last_errors)
-}
-fn eval(string:String)->ReplError{
-    todo!("Run the code! {}",string)
-}
-#[derive(Debug)]
-enum ReplError{
-    ErrorCodes(Vec<i32>),
-    SyntaxError(String)
-}
-fn repl(source:ReplSource)->Result<(),ReplError>{
-    let mut repl_err=ReplError::ErrorCodes(Vec::new());
-    loop{
-        match read(source,repl_err){
-            Some(data)=>{
-                repl_err=eval(data);
-                match source{
-                    ReplSource::File=>{
-                        match &repl_err{
-                            ReplError::ErrorCodes(codes)=>if codes.is_empty(){
-                                continue
-                            },
-                            ReplError::SyntaxError(..)=>{}
-                        }
-                        return Err(repl_err)
-                    }
-                    //The user will see the error on the next call to read, if there is one.
-                    ReplSource::User=>{}
-                }
-            },
-            None=>return Ok(())
-        }
-    }
 }
 fn run_config_file(){
     todo!("find config file and repl it")
@@ -49,7 +10,7 @@ fn run_config_file(){
 fn main() {
     let args=Arguments::parse().unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1)
+        exit(1)
     });
     if args.version{
         greet();
