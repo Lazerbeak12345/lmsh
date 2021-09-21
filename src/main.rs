8cdbc353aa63e837574ba627e86a51bcc924730c
Nathan Fritzler <nfblaster@live.com>
2021-09-21 14:33:15 -0600

    Add code to make vector of possible config file locations.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 use std::process::exit;
 use lmsh::arguments::Arguments;
 use lmsh::repl::{repl, ReplSource};
+use std::env;
+use std::path;
 fn greet(){
     println!("Welcome to Lazerbeak12345's Micro Shell!");
 }
 fn run_config_file(){
-    todo!("find config file and repl it")
+    {//find config file
+        let home_config_vec=match env::var_os("HOME"){
+            Some(val)=>{
+                let mut pb=path::PathBuf::from(val);
+                pb.push(".config");
+                vec![pb]
+            },
+            None=>Vec::new()
+        };
+        let xdg_config_dirs=match env::var_os("XDG_CONFIG_DIRS"){
+            Some(val)=>{
+                let mut vec=home_config_vec;
+                for path in env::split_paths(&val){
+                    vec.push(path)
+                }
+                vec
+            },
+            None=>home_config_vec
+        }.iter().map(|path|{
+            let mut new_path=path.clone();
+            new_path.push("lmsh");
+            new_path
+        });
+        todo!("iterate over each item in xdg_data_dirs till init.lmsh is or init.sh is found inside a folder titled \"lmsh\"")
+    }
+    //TODO repl config file
 }
 fn main() {
     let args=Arguments::parse().unwrap_or_else(|err| {
