fef7d046a88ef99a68e69756a98f670e53c68903
Nathan Fritzler <nfblaster@live.com>
2021-09-27 12:54:07 -0600

    Loosly Modularize repl.rs
    Decreases over-connectivity, pending move to seperate files.

--- src/repl.rs
+++ src/repl.rs
@@ -1,27 +1,51 @@
-use std::path::PathBuf;
-use std::io::Bytes;
-#[derive(Debug)]
-pub enum ReplSource{
-    User,
-    File(PathBuf)
+mod source{
+    use std::path::PathBuf;
+    use std::io::Bytes;
+    #[derive(Debug)]
+    pub enum ReplSource{
+        User,
+        File(PathBuf)
+    }
+    pub fn read(source:ReplSource)->Bytes<u8>{
+        todo!("Get code from the file {:?}",source)
+    }
+    pub fn prompt()->Bytes<u8>{
+        todo!("Prompt the user for code!")
+    }
 }
-fn read(source:ReplSource)->Bytes<u8>{
-    todo!("Get code from the file {:?}",source)
+pub use source::ReplSource;
+use source::*;
+mod tokens{
+    //TODO use a parse library
+    use std::io::Bytes;
+    pub enum ReplToken{}
+    #[derive(Debug)]
+    pub struct ReplTokens;
+    impl ReplTokens{
+        pub fn tokenize(bytes:Bytes<u8>)->ReplTokens{
+            todo!("convert code stream into token stream{:?}",bytes)
+        }
+    }
+    impl Iterator for ReplTokens{
+        type Item = ReplToken;
+        fn next(&mut self) -> Option<Self::Item> {
+            todo!("get the next token, if possible")
+        }
+    }
 }
-fn prompt()->Bytes<u8>{
-    todo!("Prompt the user for code!")
+use tokens::*;
+mod tree{
+    //TODO use a token library
+    use super::tokens::ReplTokens;
+    #[derive(Debug)]
+    pub struct ReplTree;
+    impl ReplTree{
+        pub fn parse(tokens:ReplTokens)->ReplTree{
+            todo!("convert token stream into parse tree{:?}",tokens)
+        }
+    }
 }
-#[derive(Debug)]
-struct ReplTokens;
-//TODO impl Iterator for ReplTokens
-fn tokenize(bytes:Bytes<u8>)->ReplTokens{
-    todo!("convert code stream into token stream{:?}",bytes)
-}
-#[derive(Debug)]
-struct ReplTree;
-fn parse(tokens:ReplTokens)->ReplTree{
-    todo!("convert token stream into parse tree{:?}",tokens)
-}
+use tree::*;
 fn eval(tree:ReplTree)->ReplReturn{
     todo!("Run the code!{:?}",tree);
 }
@@ -40,9 +64,9 @@ pub type ReplReturn=Result<(),(ReplSource,ReplError)>;
  */
 pub fn repl(source:ReplSource)->ReplReturn{
     match source{
-        ReplSource::File(..)=>eval(parse(tokenize(read(source)))),
+        ReplSource::File(..)=>eval(ReplTree::parse(ReplTokens::tokenize(read(source)))),
         ReplSource::User=>loop{
-            match eval(parse(tokenize(prompt()))){
+            match eval(ReplTree::parse(ReplTokens::tokenize(prompt()))){
                 Err(err)=>todo!("Handle error {:?}",err),
                 _=>{}
             }
